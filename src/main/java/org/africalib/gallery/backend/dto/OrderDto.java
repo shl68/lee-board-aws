627e8b2590e3f45738c1b9b7757b00a6ac2f35ed
shl68 <105649474+shl68@users.noreply.github.com>
2023-09-23 11:12:53 +0900

    Initial commit

---
+++ src/main/java/org/africalib/gallery/backend/dto/OrderDto.java
@@ -0,0 +1,12 @@
+package org.africalib.gallery.backend.dto;
+
+import lombok.Getter;
+
+@Getter
+public class OrderDto {
+
+    private String name;
+    private String address;
+    private String cardNumber;
+    private String items;
+}
